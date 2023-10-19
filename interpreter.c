0089c840777aef6781cb019a98b76e1972f126cd
Student <user@disp3269>
2023-10-19 21:45:45 +1300

    Add initial file

---
+++ interpreter.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define T int
+typedef T type_t;
+
+enum State {HALTED, USE_INTERNAL, LOAD_FILE, RUNNING, MATCHING_BRACKET}
+main(enum State s, char **argv){
+	type_t memory[]
+	return s;
+}
